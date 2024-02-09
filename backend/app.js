f1ef44beaa8b3113e2aabf7f5e4a6717a408e5ce
Aaron Anablon <>
2024-02-09 20:36:52 +0800

    zz

--- backend/app.js
+++ backend/app.js
@@ -18,33 +18,6 @@ app.use('/login', loginRouter);
 const uri = "mongodb+srv://acelmargallo04:MkfhoIf8tNp0yB7u@cluster0.yw4ydrg.mongodb.net/?retryWrites=true&w=majority";
 const port = 5000;
 
-// app.use(cors({
-//     origin: ["https://elementary-suggested.vercel.app",
-//         "https://backend-tjgb.onrender.com"]
-// }));
-
-// app.use((req, res, next) => {
-//     res.setHeader(
-//       "Access-Control-Allow-Origin",
-//       "https://elementary-suggested.vercel.app"
-//     );
-//     res.setHeader(
-//       "Access-Control-Allow-Methods",
-//       "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-//     );
-//     res.setHeader(
-//       "Access-Control-Allow-Headers",
-//       "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-//     );
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-//     res.setHeader("Access-Control-Allow-Private-Network", true);
-//     //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-//     res.setHeader("Access-Control-Max-Age", 7200);
-
-//     next();
-//   });
-
-
 app.listen(port, () => {
   console.log(`Server running on port: ${port}...`);
 });

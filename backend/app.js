958ce68ef5dfe4fc10bbf6ec9e3d0ccda4e97697
Aaron Anablon <>
2024-02-09 11:42:21 +0800

    ss

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,13 @@ const bodyParser = require('body-parser');
 const loginRouter = require('./routers/login_router')
 const mongoose = require("mongoose")
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://elementary-suggested.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
